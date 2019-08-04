734799577ec916363ccdf2c38a56c0d877627a4c
sailor <segler_alex@web.de>
2019-08-04 21:07:24 +0200

    added missing file

---
+++ src/api/data/result_message.rs
@@ -0,0 +1,27 @@
+#[derive(Serialize, Deserialize)]
+pub struct ResultMessage {
+    ok: bool,
+    message: String,
+}
+
+impl ResultMessage {
+    pub fn new(ok: bool, message: String) -> Self {
+        ResultMessage{
+            ok,
+            message
+        }
+    }
+
+    pub fn serialize_xml(&self) -> std::io::Result<String> {
+        let mut xml = xml_writer::XmlWriter::new(Vec::new());
+        xml.begin_elem("result")?;
+            xml.begin_elem("status")?;
+                xml.attr_esc("ok", &self.ok.to_string())?;
+                xml.attr_esc("message", &self.message)?;
+            xml.end_elem()?;
+        xml.end_elem()?;
+        xml.close()?;
+        xml.flush()?;
+        Ok(String::from_utf8(xml.into_inner()).unwrap_or("encoding error".to_string()))
+    }
+}
