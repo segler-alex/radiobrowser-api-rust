68333bdbb12aa1ddb289d6ab469b6b0894fc434e
sailor <segler_alex@web.de>
2020-01-14 23:12:22 +0100

    use different install paths

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo rm /usr/local/bin/radiobrowser
+sudo rm /usr/bin/radiobrowser
 sudo userdel radiobrowser
 sudo groupdel radiobrowser
 
 sudo rm /etc/systemd/system/radiobrowser.service
 sudo systemctl daemon-reload
-sudo rm -rf /usr/local/share/radiobrowser
+sudo rm -rf /usr/share/radiobrowser
