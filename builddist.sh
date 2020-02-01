5fb01cb2af259f5cf8b924aae10a2808c0e41de9
sailor <segler_alex@web.de>
2020-02-01 12:31:07 +0100

    added missing logrotate to install dist script

--- builddist.sh
+++ builddist.sh
@@ -2,14 +2,13 @@
 
 DISTDIR=$(pwd)/dist
 mkdir -p ${DISTDIR}
-mkdir -p ${DISTDIR}/etc
 mkdir -p ${DISTDIR}/bin
 cargo build --release
 
 cp target/release/radiobrowser-api-rust ${DISTDIR}/bin/radiobrowser
 cp -R init ${DISTDIR}/
 cp -R static ${DISTDIR}/
-cp radiobrowser.toml ${DISTDIR}/etc/config-example.toml
+cp -R etc ${DISTDIR}/
 cp install_from_dist.sh ${DISTDIR}/install.sh
 
 tar -czf $(pwd)/radiobrowser-dist.tar.gz -C ${DISTDIR} bin init static etc install.sh
